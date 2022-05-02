6bef936ede2b79c2bc0c667460c3d50753e1f27f
Caetano Burjack <caetano.burjack@gmail.com>
2022-05-02 19:28:15 -0300

    Inicial Commit - preparing the enviroment

---
+++ src/components/Widget.tsx
@@ -0,0 +1,11 @@
+import { ChatTeardrop } from "phosphor-react"
+
+export function Widget() {
+    return (
+        <>
+            <button className="bg-violet-500 rounded-full px-3 h-12">
+                <ChatTeardrop className="w-6 h-6" />
+            </button>
+        </>
+    )
+}
