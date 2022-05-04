18ac8a98a80c00a68fd2811d039cf3de9f381e78
Caetano Burjack <caetano.burjack@gmail.com>
2022-05-04 01:37:06 -0300

    updating some config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,19 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
 }
