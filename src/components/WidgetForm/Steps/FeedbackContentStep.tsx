9b36810fbeec7b35dd4dd0c06a3e9e7772813c6f
Caetano Burjack <caetano.burjack@gmail.com>
2022-05-06 20:23:35 -0300

    Creating axios config file

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
 import { ArrowLeft, Camera } from "phosphor-react"
 import { FormEvent, useState } from "react"
 import { FeedbackType, feedbackTypes } from ".."
+import { api } from "../../../lib/api"
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading"
 import { ScreenshotButton } from "../ScreenshotButton"
 
 interface FeedbackContentStepProps {
@@ -14,14 +16,21 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
 
     const [screenshot, setScreenshot] = useState<string | null>(null)
     const [comment, setComment] = useState('')
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false)
+
     const feedbackTypeInfo = feedbackTypes[props.feedbackType]
 
-    function handleSubmitFeedback(event: FormEvent) {
+    async function handleSubmitFeedback(event: FormEvent) {
         event.preventDefault();
-        console.log({
-            screenshot,
-            comment
+
+        setIsSendingFeedback(true)
+
+        await api.post('/feedbacks', {
+            type: props.feedbackType,
+            comment,
+            screenshot
         })
+        setIsSendingFeedback(false)
         props.onFeedbackSent()
     }
 
@@ -49,10 +58,10 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
                 <footer className="flex gap-2 mt-2">
                     <ScreenshotButton screenshot={screenshot} onScreenshotTook={setScreenshot} />
                     <button
-                        disabled={comment.length === 0}
+                        disabled={comment.length === 0 || isSendingFeedback}
                         type="submit"
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
-                        Enviar Feedback
+                        {isSendingFeedback ? <Loading /> : 'Enviar Feedback'}
                     </button>
                 </footer>
             </form>
